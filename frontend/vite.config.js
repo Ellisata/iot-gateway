a791f906c5593dd55f80145888d5b640d02fe85f
wangjun <15289288565@163.com>
2026-08-28 14:02:31 +0800

    修改前端端口

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig({
   },
 
   server: {
-    port: 3000,
+    port: 4000,
     proxy: {
       '/api': {
         target: process.env.VITE_API_BASE_URL || 'http://localhost:8080',
